8aff5fa0d35d493f5437682afc5f55218aa26d32
kj <jun111haha@gmail.com>
2022-08-28 03:12:58 +0900

    Initial commit

---
+++ src/test/java/com/mymovie/movielog/MovielogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymovie.movielog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
