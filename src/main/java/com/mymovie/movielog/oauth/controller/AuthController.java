81fb3377a326f84e91c6c0a18f6f282b58481004
kj <jun111haha@gmail.com>
2022-09-04 01:59:09 +0900

    fetch/spring security jwt

--- src/main/java/com/mymovie/movielog/oauth/controller/AuthController.java
+++ src/main/java/com/mymovie/movielog/oauth/controller/AuthController.java
@@ -1,125 +1,63 @@
 package com.mymovie.movielog.oauth.controller;
 
-import com.mymovie.movielog.common.ApiResponse;
-import com.mymovie.movielog.config.properties.AppProperties;
-import com.mymovie.movielog.oauth.entity.AuthReqModel;
-import com.mymovie.movielog.oauth.entity.RoleType;
-import com.mymovie.movielog.oauth.entity.UserPrincipal;
+import com.mymovie.movielog.common.payload.ApiResponse;
+import com.mymovie.movielog.oauth.payload.AuthRequest;
+import com.mymovie.movielog.oauth.payload.AuthResponse;
+import com.mymovie.movielog.oauth.service.AuthService;
+import com.mymovie.movielog.oauth.service.KakaoAuthService;
 import com.mymovie.movielog.oauth.token.AuthToken;
 import com.mymovie.movielog.oauth.token.AuthTokenProvider;
-import com.mymovie.movielog.user.entity.UserRefreshToken;
-import com.mymovie.movielog.user.repository.UserRefreshTokenRepository;
-import com.mymovie.movielog.utils.CookieUtil;
-import com.mymovie.movielog.utils.HeaderUtil;
-import io.jsonwebtoken.Claims;
+import com.mymovie.movielog.oauth.util.JwtHeaderUtil;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
-    private final AppProperties appProperties;
-    private final AuthTokenProvider tokenProvider;
-    private final AuthenticationManager authenticationManager;
-    private final UserRefreshTokenRepository userRefreshTokenRepository;
+    private final KakaoAuthService kakaoAuthService;
+    private final AuthTokenProvider authTokenProvider;
+    private final AuthService authService;
 
-    private final static long THREE_DAYS_MSEC = 259200000;
-    private final static String REFRESH_TOKEN = "refresh_token";
-
-    @PostMapping("/login")
-    public ApiResponse login(HttpServletRequest request, HttpServletResponse response, @RequestBody AuthReqModel authReqModel) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authReqModel.getId(), authReqModel.getPassword()));
-
-        String userId = authReqModel.getId();
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        Date now = new Date();
-        AuthToken accessToken = tokenProvider.createAuthToken(userId, ((UserPrincipal) authentication.getPrincipal()).getRoleType().getCode(), new Date(now.getTime() + appProperties.getAuth().getTokenExpiry()));
-
-        long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpiry();
-        AuthToken refreshToken = tokenProvider.createAuthToken(appProperties.getAuth().getTokenSecret(), new Date(now.getTime() + refreshTokenExpiry));
-
-        // userId refresh token 으로 DB 확인
-        UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUserId(userId);
-        if (userRefreshToken == null) {
-            // 없는 경우 새로 등록
-            userRefreshToken = new UserRefreshToken(userId, refreshToken.getToken());
-            userRefreshTokenRepository.saveAndFlush(userRefreshToken);
-        } else {
-            // DB에 refresh 토큰 업데이트
-            userRefreshToken.setRefreshToken(refreshToken.getToken());
-        }
-
-        int cookieMaxAge = (int) refreshTokenExpiry / 60;
-        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
-        CookieUtil.addCookie(response, REFRESH_TOKEN, refreshToken.getToken(), cookieMaxAge);
-
-        return ApiResponse.success("token", accessToken.getToken());
+    /*
+    카카오 소셜 로그인 기능
+    @return ResponseEntity<AuthResponse>
+     */
+    @ApiOperation(value = "카카오 로그인", notes = "카카오 액세스 토큰을 이용해 사용자 정보 받고 저장해 앱의 토큰 반환")
+    @PostMapping(value = "/kakao")
+    public ResponseEntity<AuthResponse> kakaoAuthRequest(@RequestBody AuthRequest authRequest) {
+        return ApiResponse.success(kakaoAuthService.login(authRequest)); // body에 appToken 반환(response code 200)
     }
 
+    /*
+    appToken 갱신
+    @return ResponseEntity<AuthResponse>
+     */
+    @ApiOperation(value = "appToken 갱신", notes = "appToken 갱신")
     @GetMapping("/refresh")
-    public ApiResponse refreshToken(HttpServletRequest request, HttpServletResponse response) {
-        // access token 확인
-        String accessToken = HeaderUtil.getAccessToken(request);
-        AuthToken authToken = tokenProvider.convertAuthToken(accessToken);
-        if (!authToken.validate()) {
-            return ApiResponse.invalidAccessToken();
+    public ResponseEntity<AuthResponse> refreshToken(HttpServletRequest request) {
+        String appToken = JwtHeaderUtil.getAccessToken(request);
+        AuthToken authToken = authTokenProvider.convertAuthToken(appToken);
+
+        if (!authToken.validate()) { // 형식에 맞지 않는 token
+            return ApiResponse.forbidden(null); // body에 담은 것 없이, 403 HTTP code return
         }
 
-        // expired access token 인지 확인
-        Claims claims = authToken.getExpiredTokenClaims();
-        if (claims == null) {
-            return ApiResponse.notExpiredTokenYet();
+        AuthResponse authResponse = authService.updateToken(authToken);
+
+        if (authResponse == null) { // token 만료
+            return ApiResponse.forbidden(null); // body에 담은 것 없이, 403 HTTP code return
         }
 
-        String userId = claims.getSubject();
-        RoleType roleType = RoleType.of(claims.get("role", String.class));
-
-        // refresh token
-        String refreshToken = CookieUtil.getCookie(request, REFRESH_TOKEN).map(Cookie::getValue).orElse((null));
-        AuthToken authRefreshToken = tokenProvider.convertAuthToken(refreshToken);
-
-        if (authRefreshToken.validate()) {
-            return ApiResponse.invalidRefreshToken();
-        }
-
-        // userId refresh token 으로 DB 확인
-        UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByUserIdAndRefreshToken(userId, refreshToken);
-        if (userRefreshToken == null) {
-            return ApiResponse.invalidRefreshToken();
-        }
-
-        Date now = new Date();
-        AuthToken newAccessToken = tokenProvider.createAuthToken(userId, roleType.getCode(), new Date(now.getTime() + appProperties.getAuth().getTokenExpiry()));
-
-        long validTime = authRefreshToken.getTokenClaims().getExpiration().getTime() - now.getTime();
-
-        // refresh 토큰 기간이 3일 이하로 남은 경우, refresh 토큰 갱신
-        if (validTime <= THREE_DAYS_MSEC) {
-            // refresh 토큰 설정
-            long refreshTokenExpiry = appProperties.getAuth().getRefreshTokenExpiry();
-
-            authRefreshToken = tokenProvider.createAuthToken(appProperties.getAuth().getTokenSecret(), new Date(now.getTime() + refreshTokenExpiry));
-
-            // DB에 refresh 토큰 업데이트
-            userRefreshToken.setRefreshToken(authRefreshToken.getToken());
-
-            int cookieMaxAge = (int) refreshTokenExpiry / 60;
-            CookieUtil.deleteCookie(request, response, REFRESH_TOKEN);
-            CookieUtil.addCookie(response, REFRESH_TOKEN, authRefreshToken.getToken(), cookieMaxAge);
-        }
-
-        return ApiResponse.success("token", newAccessToken.getToken());
+        return ApiResponse.success(authResponse);
     }
+
 }
